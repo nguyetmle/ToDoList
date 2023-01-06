d2a6c580a54cc75422e0ad2f4c8946c93eda791e
nguyetmle <leminhnguyet2809@gmail.com>
2023-01-06 15:07:18 -0500

    refactor codes

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public")); //tell express to serve up public folder (and its static resources)
 
-var items = [];
+const items = [];
 
 app.get("/", function(req,res){
-    var today = new Date();
-    
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
+    const day = date.getDay();
     res.render("list", {kindOfDay: day, newListItems: items});
 
 });
 
 app.post("/", function(req,res){
-    var item = req.body.newItem;
+    const item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 })
